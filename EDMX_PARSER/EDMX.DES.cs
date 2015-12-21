c755486b4299c231511a0a5861b27bc6f4672c83
eggy_lumbres@yahoo.com <eggy_lumbres@yahoo.com>
2015-12-20 22:40:22 -0600

    12-20-2015

---
+++ EDMX_PARSER/EDMX.DES.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDMX_PARSER.Wrapper;
+using EDMX_PARSER.Serialize;
+
+namespace EDMX_PARSER.DES
+{
+    class EDMX : EDMX_PARSER.Wrapper.Edmx
+    {
+
+        public EDMX() : base()
+        {
+            var EntityType = GetConceptualModelEntityType();
+            var AssociationSet = GetStorageModelEntityContainer().AssociationSet;
+            var Association = GetStorageModelSchema().Association;
+            var SEntityType = GetStorageModelEntityType();
+
+            var address = GetEntity("Address");
+
+            var result = from nav in address.NavigationProperty
+                         join assoc in AssociationSet
+                         on StripLastName(nav.Relationship) equals assoc.Name
+                         select new
+                         {
+                             address.Name,
+                             nav.Relationship,
+                             nav.FromRole,
+                             nav.ToRole,
+                             assoc.End
+                         };
+
+            var r = //from EntType in EntityType
+                    from NavProp in GetEntity("Address").NavigationProperty
+                    join EntAssoc in Association
+                    on StripLastName(NavProp.Relationship) equals EntAssoc.Name
+                    from Role in EntAssoc.End
+                    join Db in SEntityType
+                    on StripLastName(Role.Type) equals Db.Name
+                    into res
+                    select res;
+        }
+
+        
+        public SchemaEntityType1 GetEntity(string name)
+        {
+            var entitytype = GetConceptualModelEntityType();
+
+            var result = (from entity in entitytype
+                          where entity.Name == name
+                          select entity).First();
+
+            return (EDMX_PARSER.Serialize.SchemaEntityType1)result;
+        }
+
+        /*public IEnumerable<SchemaEntityTypeNavigationProperty> GetRelatedEntity(string name)
+        {
+            var entity = GetEntity(name);
+            var result = entity.NavigationProperty;
+
+            return (IEnumerable<SchemaEntityTypeNavigationProperty>)result;
+        }
+
+        public SchemaAssociation GetRelatedEntityAssociation(string relationship)
+        {
+            string identifier = StripLastName(relationship);
+
+            var association = (from assoc in this.Associations
+                               where assoc.Name == identifier
+                               select assoc).First();
+
+            return association;
+        }*/
+
+        public string StripLastName(string relationship)
+        {
+            string[] names = relationship.Split('.');
+            string identifier = names[names.Length - 1];
+            return identifier;
+        }
+        
+
+    }
+}
---
+++ EDMX_PARSER/EDMX.Wrapper.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using EDMX_PARSER;
+using System.Configuration;
+using EDMX_PARSER.Serialize;
+
+namespace EDMX_PARSER.Wrapper
+{
+    public partial class Edmx
+    {
+        public EDMX_PARSER.Serialize.Edmx edmx;
+
+        public Edmx()
+        {
+            String fullFileName = ConfigurationManager.AppSettings["FullFilePath"];
+            XmlSerializer deserializer = new XmlSerializer(typeof(EDMX_PARSER.Serialize.Edmx));
+            TextReader reader = new StreamReader(fullFileName);
+            object obj = deserializer.Deserialize(reader);
+            this.edmx = (EDMX_PARSER.Serialize.Edmx)obj;
+            reader.Close();
+        }
+
+        #region Level 1 - edmx
+
+        public EdmxDesigner GetDesigner()
+        {
+            return edmx.Designer;
+        }
+
+        public EdmxRuntime GetRuntime()
+        {
+            return edmx.Runtime;
+        }
+
+        #endregion
+        
+        #region Level 2 - models and mappings
+
+        public EdmxRuntimeStorageModels GetStorageModel()
+        {
+            return GetRuntime().StorageModels;
+        }
+
+        public EdmxRuntimeConceptualModels GetConceptualModel()
+        {
+            return GetRuntime().ConceptualModels;
+        }
+
+        public EdmxRuntimeMappings GetMappings()
+        {
+            return GetRuntime().Mappings;
+        }
+
+        #endregion
+
+        #region Level 3 - schema and mapping
+
+        public Schema GetStorageModelSchema()
+        {
+            return GetStorageModel().Schema;
+        }
+
+        public Schema1 GetConceptualModelSchema()
+        {
+            return GetConceptualModel().Schema;
+        }
+
+        public Mapping GetMappingsMapping()
+        {
+            return GetMappings().Mapping;
+        }
+
+        #endregion
+
+        #region Level 4 - Model Contents
+
+        //Level 4 - Storage Model
+        public SchemaEntityContainer GetStorageModelEntityContainer()
+        {
+            return GetStorageModelSchema().EntityContainer;
+        }
+
+        public SchemaEntityType[] GetStorageModelEntityType()
+        {
+            return GetStorageModelSchema().EntityType;
+        }
+
+        public SchemaAssociation[] GetStorageModelAssociation()
+        {
+            return GetStorageModelSchema().Association;
+        }
+
+        public SchemaFunction[] GetStorageModelFunction()
+        {
+            return GetStorageModelSchema().Function;
+        }
+
+        //Level 4 - Conceptual Model
+        public SchemaEntityContainer1 GetConceptualModelEntityContainer()
+        {
+            return GetConceptualModelSchema().EntityContainer;
+        }
+
+        public SchemaEntityType1[] GetConceptualModelEntityType()
+        {
+            return GetConceptualModelSchema().EntityType;
+        }
+
+        public SchemaAssociation1[] GetConceptualModelAssociation()
+        {
+            return GetConceptualModelSchema().Association;
+        }
+
+        //Level 4 - Mapping
+        public MappingEntityContainerMapping GetMappingEntityContainerMapping()
+        {
+            return GetMappingsMapping().EntityContainerMapping;
+        }
+
+        public MappingEntityContainerMappingEntitySetMapping[] GetMappingEntitySetMapping()
+        {
+            return GetMappingEntityContainerMapping().EntitySetMapping;
+        }
+
+        public MappingEntityContainerMappingFunctionImportMapping[] GetMappingFunctionImportMapping()
+        {
+            return GetMappingEntityContainerMapping().FunctionImportMapping;
+        }
+
+        #endregion
+
+    }
+}
